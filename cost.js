dc65949331de3a84c72257b327afb6bf534909a9
Anderson-Copper <duquesne.pierre@outlook.com>
2025-06-06 03:57:15 +0200

    Update cost.js

--- cost.js
+++ cost.js
@@ -2,7 +2,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { DateTime } = require('luxon');
 
-const COST_CHANNEL_ID = '1375152581307007056'; // mis à jour pour Logs-Dépôts-Livraison
+const COST_CHANNEL_ID = '1379479480208461884'; // mis à jour pour Logs-Dépôts-Livraison
 
 function getWeekDateRange(weekCode) {
   const match = weekCode.match(/^S(\d{1,2})$/i);
